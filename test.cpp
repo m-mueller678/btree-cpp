45debf31589f8c9b43f151f9fa3b7bab78a4ea0a
Marcus Müller <marcus.mueller@cit.tum.de>
2023-08-10 12:55:06 +0200

    measure hash perf

--- test.cpp
+++ test.cpp
@@ -19,76 +19,19 @@ void runTest(PerfEvent& e, vector<string>& data)
    e.setParam("type", "btr");
    e.setParam("factr", "0");
    e.setParam("base", "0");
+
+   uint64_t total = 0;
    {
       // insert
-      e.setParam("op", "insert");
-      PerfEventBlock b(e, count);
-      for (uint64_t i = 0; i < count; i++) {
-         t.insert((uint8_t*)data[i].data(), data[i].size(), reinterpret_cast<uint8_t*>(&i), sizeof(uint64_t));
-
-         // for (uint64_t j=0; j<=i; j+=1) if (!t.lookup((uint8_t*)data[j].data(), data[j].size())) throw;
-         // for (uint64_t j=0; j<=i; j++) if (!t.lookup((uint8_t*)data[j].data(), data[j].size()-8)) throw;
+      e.setParam("op", "hash");
+      PerfEventBlock b(e, count * 3);
+      for (unsigned i = 0; i < 3; ++i) {
+         for (uint64_t i = 0; i < count; i++) {
+            total += HashNode::compute_hash((uint8_t*)data[i].data(), data[i].size());
+         }
       }
    }
-
-   {
-      // lookup
-      e.setParam("op", "lookup");
-      PerfEventBlock b(e, count);
-      for (uint64_t i = 0; i < count; i++) {
-         unsigned payloadSize;
-         uint8_t* payload = t.lookup((uint8_t*)data[i].data(), data[i].size(), payloadSize);
-         if (!payload || (payloadSize != sizeof(uint64_t)) || *reinterpret_cast<uint64_t*>(payload) != i)
-            throw;
-      }
-   }
-
-   {
-      e.setParam("op", "range");
-      PerfEventBlock b(e, count / 4);
-      for (uint64_t i = 0; i < count; i += 4) {
-         uint8_t keyBuffer[BTreeNode::maxKVSize];
-         unsigned foundIndex = 0;
-         t.range_lookup((uint8_t*)data[i].data(), data[i].size(), keyBuffer, [&](unsigned keyLen, uint8_t* payload, unsigned payloadLen) {
-            assert(payloadLen == sizeof(uint64_t));
-            uint64_t loadedPayload = loadUnaligned<uint64_t>(payload);
-            assert(data[loadedPayload].size() == keyLen);
-            assert(memcmp(keyBuffer, data[loadedPayload].data(), keyLen) == 0);
-            foundIndex += 1;
-            return foundIndex < 10;
-         });
-         if (foundIndex > 10 || (foundIndex < 10 && foundIndex + i != data.size()))
-            throw;
-      }
-   }
-
-   // prefix lookup
-   for (uint64_t i = 0; i < count; i++)
-      t.lookup((uint8_t*)data[i].data(), data[i].size() - (data[i].size() / 4));
-
-   {
-      for (uint64_t i = 0; i < count; i += 4)  // remove some
-         if (!t.remove((uint8_t*)data[i].data(), data[i].size()))
-            throw;
-      for (uint64_t i = 0; i < count; i++)  // lookup all, causes some misses
-         if ((i % 4 == 0) == t.lookup((uint8_t*)data[i].data(), data[i].size()))
-            throw;
-      for (uint64_t i = 0; i < count / 2 + count / 4; i++)  // remove some more
-         if ((i % 4 == 0) == t.remove((uint8_t*)data[i].data(), data[i].size()))
-            throw;
-      for (uint64_t i = 0; i < count / 2 + count / 4; i++)  // insert
-         t.insert((uint8_t*)data[i].data(), data[i].size(), reinterpret_cast<uint8_t*>(&i), sizeof(uint64_t));
-      {
-         e.setParam("op", "remove");
-         PerfEventBlock b(e, count);
-         for (uint64_t i = 0; i < count; i++)  // remove all
-            t.remove((uint8_t*)data[i].data(), data[i].size());
-      }
-      for (uint64_t i = 0; i < count; i++)
-         if (t.lookup((uint8_t*)data[i].data(), data[i].size()))
-            throw;
-   }
-
+   printf("%lu", total);
    data.clear();
 }
 
