2edc046e3982ebbc0c6c16d67b850de3247e78d7
Marcus Müller <marcus.mueller@cit.tum.de>
2023-08-21 16:11:02 +0200

    implement ycsb-D. Broken.

--- ycsb2.cpp
+++ ycsb2.cpp
@@ -14,7 +14,8 @@ ZipfGenerator* zipf_init_generator(uint32_t, double);
 void zipf_generate(ZipfGenerator*, uint32_t*, uint32_t);
 }
 
-constexpr unsigned ZIPF_GEN_SIZE = 1 << 25;
+constexpr unsigned ZIPF_GEN_SIZE = 1 << 23;
+constexpr unsigned OP_GEN_SIZE = ZIPF_GEN_SIZE << 2;
 constexpr double ZIPF_PARAMETER = 1.5;
 
 uint64_t envu64(const char* env)
@@ -25,26 +26,51 @@ uint64_t envu64(const char* env)
    abort();
 }
 
-void runTest(BTreeCppPerfEvent e, vector<string>& data, unsigned payloadSize, unsigned opCount)
+unsigned* makeZipfIndexArray(unsigned genSize, unsigned keyCount)
 {
-   unsigned keyCount = data.size();
-
-   unsigned* zipf_indices = new unsigned[ZIPF_GEN_SIZE];
+   unsigned* zipfIndices = new unsigned[genSize];
    if (keyCount > 0) {
       ZipfGenerator* generator = zipf_init_generator(keyCount, ZIPF_PARAMETER);
-      zipf_generate(generator, zipf_indices, ZIPF_GEN_SIZE);
-   } else if (opCount != 0) {
-      std::cerr << "no keys" << std::endl;
-      throw;
+      zipf_generate(generator, zipfIndices, genSize);
+   }
+   return zipfIndices;
+}
+
+bool* makeOpArray(unsigned genSize, double trueRate)
+{
+   bool* op = new bool[genSize];
+   unsigned trueCount = lround(genSize * trueRate);
+   for (unsigned i = 0; i < genSize; ++i)
+      op[i] = i < trueCount;
+   random_shuffle(op, op + genSize);
+   return op;
+}
+
+uint8_t* makePayload(unsigned len)
+{
+   uint8_t* payload = new uint8_t[len];
+   memset(payload, 42, len);
+   return payload;
+}
+
+void runYcsbC(BTreeCppPerfEvent e, vector<string>& data, unsigned keyCount, unsigned payloadSize, unsigned opCount)
+{
+   if (keyCount <= data.size()) {
+      random_shuffle(data.begin(), data.end());
+      data.resize(keyCount);
+   } else {
+      std::cerr << "not enough keys" << std::endl;
+      keyCount = 0;
+      opCount = 0;
    }
 
-   uint8_t payload[payloadSize];
-   memset(payload, 42, payloadSize);
+   uint8_t* payload = makePayload(payloadSize);
+   unsigned* zipf_indices = makeZipfIndexArray(ZIPF_GEN_SIZE, keyCount);
 
    DataStructureWrapper t;
    {
       // insert
-      e.setParam("op", "insert");
+      e.setParam("op", "ycsb_c_init");
       BTreeCppPerfEventBlock b(e, keyCount);
       for (uint64_t i = 0; i < keyCount; i++) {
          uint8_t* key = (uint8_t*)data[i].data();
@@ -54,7 +80,7 @@ void runTest(BTreeCppPerfEvent e, vector<string>& data, unsigned payloadSize, un
    }
 
    {
-      e.setParam("op", "lookup");
+      e.setParam("op", "ycsb_c");
       BTreeCppPerfEventBlock b(e, opCount);
       for (uint64_t i = 0; i < opCount; i++) {
          unsigned keyIndex = zipf_indices[i % ZIPF_GEN_SIZE] - 1;
@@ -69,6 +95,71 @@ void runTest(BTreeCppPerfEvent e, vector<string>& data, unsigned payloadSize, un
    }
 
    data.clear();
+}
+
+void runYcsbD(BTreeCppPerfEvent e, vector<string>& data, unsigned keyCount, unsigned payloadSize, unsigned opCount)
+{
+   if (keyCount < opCount / 40) {
+      std::cerr << "too many ops for data size" << std::endl;
+   }
+   unsigned initialKeyCount = keyCount - opCount / 40;
+   unsigned expectedInsertions = opCount / 20;
+   if (initialKeyCount + expectedInsertions * 2 > data.size()) {
+      std::cerr << "not enough keys" << std::endl;
+   }
+
+   random_shuffle(data.begin(), data.end());
+   uint8_t* payload = makePayload(payloadSize);
+   unsigned* zipf_indices = makeZipfIndexArray(ZIPF_GEN_SIZE, data.size());
+   bool* op_array = makeOpArray(OP_GEN_SIZE, 0.05);
+
+   DataStructureWrapper t;
+   {
+      // insert
+      e.setParam("op", "ycsb_d_init");
+      BTreeCppPerfEventBlock b(e, initialKeyCount);
+      for (uint64_t i = 0; i < initialKeyCount; i++) {
+         uint8_t* key = (uint8_t*)data[i].data();
+         unsigned int length = data[i].size();
+         t.insert(key, length, payload, payloadSize);
+      }
+   }
+
+   unsigned insertedCount = initialKeyCount;
+   unsigned sampleIndex = 0;
+   {
+      e.setParam("op", "ycsb_d");
+      BTreeCppPerfEventBlock b(e, opCount);
+      for (uint64_t completedOps = 0; completedOps < opCount; ++completedOps, ++sampleIndex) {
+         if (op_array[sampleIndex % OP_GEN_SIZE]) {
+            if (insertedCount == data.size()) {
+               std::cerr << "exhausted keys for insertion" << std::endl;
+               abort();
+            }
+            uint8_t* key = (uint8_t*)data[insertedCount].data();
+            unsigned int length = data[insertedCount].size();
+            t.insert(key, length, payload, payloadSize);
+            ++insertedCount;
+         } else {
+            while (true) {
+               unsigned zipfSample = zipf_indices[sampleIndex % ZIPF_GEN_SIZE] - 1;
+               if (zipfSample < insertedCount) {
+                  unsigned keyIndex = insertedCount - zipfSample;
+                  unsigned payloadSizeOut;
+                  uint8_t* key = (uint8_t*)data[keyIndex].data();
+                  unsigned long length = data[keyIndex].size();
+                  uint8_t* payload = t.lookup(key, length, payloadSizeOut);
+                  if (!payload || (payloadSize != payloadSizeOut) || *payload != 42)
+                     throw;
+                  break;
+               } else {
+                  std::cerr << "rejected zipf sample" << std::endl;
+                  ++sampleIndex;
+               }
+            }
+         }
+      }
+   }
 }
 
 int main()
@@ -119,14 +210,22 @@ int main()
    e.setParam("payload_size", payloadSize);
    e.setParam("run_id", envu64("RUN_ID"));
 
-   if (keyCount <= data.size()) {
-      random_shuffle(data.begin(), data.end());
-      data.resize(keyCount);
-      runTest(e, data, payloadSize, opCount);
-   } else {
-      std::cerr << "not enough keys in " << keySet << std::endl;
-      data.resize(0);
-      runTest(e, data, 0, 0);
+   unsigned maxExpectedKeyCount = keyCount + opCount / 20;
+
+   switch (envu64("YCSB_VARIANT")) {
+      case 3: {
+         runYcsbC(e, data, keyCount, payloadSize, opCount);
+         break;
+      }
+      case 4: {
+         runYcsbD(e, data, keyCount, payloadSize, opCount);
+         break;
+      }
+      default: {
+         std::cerr << "bad ycsb variant";
+         abort();
+      }
    }
+
    return 0;
 }
