b77f47191436a097618c9b3a1b658279a594a190
Marcus Müller <marcus.mueller@cit.tum.de>
2023-08-22 16:09:27 +0200

    basic art vs btree analysis

---
+++ R/art-btree-initial/R.R
@@ -0,0 +1,40 @@
+library(ggplot2)
+library(sqldf)
+
+CONFIG_NAMES = c('baseline', 'prefix', 'heads', 'hints', 'hash', 'dense', 'inner', 'art')
+
+r = read.csv('d1.csv', strip.white = TRUE)
+r$config_name = ordered(r$config_name, levels = CONFIG_NAMES, labels = CONFIG_NAMES)
+
+ggplot(sqldf('
+select * from r
+where true
+and op="ycsb_c"
+--and payload_size=8
+-- and data_size=1000000
+and run_id=1
+--and config_name="baseline"
+--and data_name="file:data/urls"
+--and (config_name in ("hints","hash","dense","inner"))
+')) +
+  facet_grid(data_name ~ config_name) +
+  geom_line(aes(payload_size, scale / time)) +
+  #scale_y_log10()+
+  expand_limits(y = 0) +
+  scale_x_log10()
+
+ggplot(sqldf('
+select * from r
+where true
+--and op="ycsb_c"
+and payload_size=8
+-- and data_size=1000000
+and run_id=1
+--and config_name="baseline"
+--and data_name="file:data/urls"
+--and (config_name in ("hints","hash","dense","inner"))
+')) +
+  facet_grid(data_name ~ op) +
+  geom_col(aes(config_name, scale / time)) +
+  #scale_y_log10()+
+  expand_limits(y = 0)
