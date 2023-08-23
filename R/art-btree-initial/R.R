f7bb37dbf80513520d1ddb81fd8d247ec88424be
Marcus Müller <marcus.mueller@cit.tum.de>
2023-08-23 11:03:13 +0200

    nested facets

--- R/art-btree-initial/R.R
+++ R/art-btree-initial/R.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(sqldf)
+library(ggh4x)
 
 CONFIG_NAMES = c('baseline', 'prefix', 'heads', 'hints', 'hash', 'dense', 'inner', 'art')
 
@@ -9,17 +10,15 @@ r$config_name = ordered(r$config_name, levels = CONFIG_NAMES, labels = CONFIG_NA
 ggplot(sqldf('
 select * from r
 where true
-and op="ycsb_c"
+and op in ("ycsb_c","ycsb_d")
 --and payload_size=8
 and data_size=1000000
 and run_id=1
 --and config_name="hints"
 --and data_name="data/urls"
 ')) +
-  facet_grid(data_name ~ config_name) +
+  facet_nested(data_name~op + config_name,scales = 'free_y') +
   geom_line(aes(payload_size, scale / time)) +
-  #scale_y_log10()+
-  expand_limits(y = 0) +
   scale_x_log10()
 
 ggplot(sqldf('
