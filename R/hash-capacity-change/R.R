d7eeda13fb9b1b25b3d6569a23d368e75401107b
Marcus Müller <marcus.mueller@cit.tum.de>
2023-08-25 15:06:10 +0200

    hash R

--- R/hash-capacity-change/R.R
+++ R/hash-capacity-change/R.R
@@ -4,8 +4,10 @@ library(ggh4x)
 library(dplyr)
 
 CONFIG_NAMES = c('baseline', 'prefix', 'heads', 'hints', 'hash', 'dense', 'inner', 'art')
+HASH_NAMES = c('old', 'new', 'lazy', 'space')
 
 #parallel -j80% --memfree 16G --retries -1 --joblog joblog -- KEY_COUNT={1} PAYLOAD_SIZE=8 DATA={2} OP_COUNT=5e6 RUN_ID={3} SCAN_LENGTH=0 YCSB_VARIANT=4 {4} ::: 1e6 2e6 3e6 4e6 5e6 ::: int data/* ::: $(seq 1 30) ::: ./hash-* > hash-cap.csv
+# collected over multiple runs
 rr = read.csv('d1.csv', strip.white = TRUE)
 rr <- rr %>%
   mutate(avgKeySize = case_when(
@@ -25,27 +27,35 @@ rr <- rr %>%
     TRUE ~ NA
   ))
 
+rr$config_name = ordered(rr$config_name, levels = CONFIG_NAMES, labels = CONFIG_NAMES)
+rr <- rr %>% select(-starts_with("const"))
+rr$bin_name = ordered(rr$bin_name, levels = HASH_NAMES, labels = HASH_NAMES)
 
-r <- rr %>% select(-starts_with("const"))
 r = sqldf('select * from r where data_name!="data/genome"')
-r$config_name = ordered(r$config_name, levels = CONFIG_NAMES, labels = CONFIG_NAMES)
 
 
 ggplot(sqldf('
 select * from r
 where true
-and op="ycsb_d_init"
 ')) +
   facet_nested(data_name + op ~ ., scales = 'free_y') +
-  #geom_smooth(aes(data_size, scale / time, col = bin_name)) +
-  geom_line(aes(data_size, scale / time, col = bin_name), stat = 'summary', fun = mean)
+  geom_smooth(aes(data_size, scale / time, col = bin_name,linetype = bin_name))
+  #geom_line(aes(data_size, scale / time, col = bin_name), stat = 'summary', fun = mean)
 
 ggplot(sqldf('
 select * from r
 where true
-and op="ycsb_d_init"
 ')) +
   facet_nested(data_name + op ~ ., scales = 'free_y') +
-  #geom_smooth(aes(data_size, scale / time, col = bin_name))
-  geom_line(aes(data_size, scale / time, col = bin_name), stat = 'summary', fun = mean)
-#geom_line(aes(data_size, instr, col = bin_name),stat='summary',fun=mean)
+  geom_smooth(aes(data_size, instr, col = bin_name,linetype =bin_name))
+  #geom_line(aes(data_size, instr, col = bin_name), stat = 'summary', fun = mean)
+  #geom_line(aes(data_size, instr, col = bin_name),stat='summary',fun=mean)
+
+ggplot(sqldf('
+select * from r
+where true
+')) +
+  facet_nested(data_name + op ~ ., scales = 'free_y') +
+  geom_smooth(aes(data_size, L1_miss, col = bin_name,linetype =bin_name))
+#geom_line(aes(data_size, L1_miss, col = bin_name), stat = 'summary', fun = mean)
+#geom_line(aes(data_size, L1_miss, col = bin_name),stat='summary',fun=mean)
