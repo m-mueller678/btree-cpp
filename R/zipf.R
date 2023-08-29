aeed72b728ef6eeb0058bf9a2082d3e9c5bab544
Marcus Müller <marcus.mueller@cit.tum.de>
2023-08-29 14:57:01 +0200

    zipf plot

--- R/zipf.R
+++ R/zipf.R
@@ -6,8 +6,23 @@ library(dplyr)
 
 r = read.csv('../zipf_samples.csv',header=FALSE)
 colnames(r) <- c('z')
-data = head(r, n = 1000000)
+data = head(r, n = 1000)
 
 ggplot(data, aes(z)) +
   stat_ecdf(data=data) +
   coord_cartesian(xlim=c(0, 10000))
+
+
+
+ggplot(sqldf('
+select z,count(*) as count,DENSE_RANK() OVER (ORDER BY count(*) DESC) AS rank
+from data
+group by z
+order by count(*) DESC
+'))+
+  geom_line(aes(rank,count/length(data$z)))
+coord_cartesian(xlim=c(0, 100))
+
+
+
+
