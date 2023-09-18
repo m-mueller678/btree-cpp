e416aa0c8375e03046fa40e88c86f651f0e624cb
Marcus Müller <marcus.mueller@cit.tum.de>
2023-09-18 14:23:08 +0200

    conclude zipf

--- R/zipf/zipf.R
+++ R/zipf/zipf.R
@@ -31,33 +31,33 @@ d <- sqldf('select d.*,d2.* from d join d as d2 using (run_id,op,data_name,confi
 names(d)[(ncol(d) - ncol(d)/2 + 1):ncol(d)] <- paste("z0_", tail(names(d), ncol(d)/2), sep = "")
 
 ggplot(d) +
-  scale_fill_hue()+
-  facet_nested(config_name~data_name, scales = 'free') +
-  geom_line(aes(ycsb_zipf, scale / time,col=op), stat = "summary", fun = mean) +
+  scale_color_hue()+
+  facet_nested(op~data_name, scales = 'free') +
+  geom_line(aes(ycsb_zipf, scale / time,col=config_name), stat = "summary", fun = mean) +
   expand_limits(y = 0) +
   scale_y_continuous(labels = format_si(), expand = c(0, 1e5)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-ggplot(sqldf('select * from d where op="ycsb_c"')) +
-  scale_fill_hue()+
-  facet_nested(data_name~., scales = 'free') +
-  geom_line(aes(ycsb_zipf, scale / time - z0_scale/z0_time,col=config_name), stat = "summary", fun = mean) +
+ggplot(sqldf('select * from d ')) +
+  scale_color_hue()+
+  facet_nested(op~data_name, scales = 'free') +
+  geom_line(aes(ycsb_zipf, z0_time/time,col=config_name), stat = "summary", fun = mean) +
   expand_limits(y = 0) +
-  scale_y_continuous(labels = format_si(), expand = c(0, 1e5)) +
+  scale_y_log10() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 ggplot(sqldf('select * from d where op="ycsb_e"')) +
-  scale_fill_hue()+
+  scale_color_hue()+
   facet_nested(data_name~., scales = 'free') +
   geom_line(aes(ycsb_zipf, scale/time,col=config_name), stat = "summary", fun = mean) +
   expand_limits(y = 0) +
   scale_y_continuous(labels = format_si(), expand = c(0, 1e5)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-ggplot(sqldf('select * from d')) +
+ggplot(sqldf('select * from d ')) +
   scale_color_hue()+
-  facet_nested(data_name~., scales = 'free') +
-  geom_line(aes(ycsb_zipf, (scale / time) / (z0_scale/z0_time),col=config_name), stat = "summary", fun = mean) +
+  facet_nested(data_name~op) +
+  geom_line(aes(ycsb_zipf, time / z0_time,col=config_name), stat = "summary", fun = mean) +
   expand_limits(y = 0) +
   scale_y_log10(labels = format_si()) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
@@ -73,8 +73,45 @@ ggplot(sqldf('select * from d')) +
 
 ggplot(sqldf('select * from d')) +
   scale_color_hue()+
-  facet_nested(data_name~., scales = 'free') +
-  geom_line(aes(ycsb_zipf, LLC_miss/z0_LLC_miss,col=config_name), stat = "summary", fun = mean) +
+  facet_nested(op+data_name~config_name) +
+  geom_line(aes(ycsb_zipf, z0_br_miss/br_miss),col='green', stat = "summary", fun = mean) +
+  geom_line(aes(ycsb_zipf, z0_L1_miss/L1_miss),col='red', stat = "summary", fun = mean) +
+  geom_line(aes(ycsb_zipf, z0_LLC_miss/LLC_miss),col='blue', stat = "summary", fun = mean) +
+  geom_line(aes(ycsb_zipf, z0_cycle/cycle),col='yellow', stat = "summary", fun = mean) +
   expand_limits(y = 0) +
   scale_y_log10(labels = format_si()) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggplot(sqldf('select * from d where data_name="int" and op="ycsb_c"')) +
+  scale_color_hue()+
+  facet_nested(.~config_name) +
+  geom_line(aes(ycsb_zipf, z0_br_miss/br_miss),col='green', stat = "summary", fun = mean) +
+  geom_line(aes(ycsb_zipf, z0_L1_miss/L1_miss),col='red', stat = "summary", fun = mean) +
+  geom_line(aes(ycsb_zipf, z0_LLC_miss/LLC_miss),col='blue', stat = "summary", fun = mean) +
+  geom_line(aes(ycsb_zipf, z0_cycle/cycle),col='yellow', stat = "summary", fun = mean) +
+  geom_line(aes(ycsb_zipf, IPC),col='purple', stat = "summary", fun = mean) +
+  geom_line(aes(ycsb_zipf, instr/100),col='cyan', stat = "summary", fun = mean) +
+  expand_limits(y = 0) +
+  #scale_y_log10(labels = format_si()) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# TODO why is hint array so good for scans?
+ggplot(fetch2(d,'config_name','heads','hints','op,data_name,ycsb_zipf','scale/time')) +
+  scale_color_hue(
+    labels = c("int" = "ints", "data/urls" = "urls", "data/wiki" = "wiki")
+  )+
+  facet_nested(op~., scales = 'free',labeller = function(x){
+    return(x %>% mutate(op = case_when(
+      op=='ycsb_c'~"YCSB-C",
+      op=='ycsb_e'~"YCSB-E",
+      TRUE~'ERROR'
+    )))
+  }) +
+  geom_smooth(aes(ycsb_zipf, b/a-1,col=data_name)) +
+  geom_line(aes(ycsb_zipf, b/a-1,col=data_name),) +
+  expand_limits(y = 0) +
+  scale_y_continuous(labels=percent_format(style_positive = "plus")) +
+  xlab('α')+
+  ylab(NULL)+
+  labs(col = "key set")+
+  theme(legend.justification = 'right',legend.position = c(1,0.3))
