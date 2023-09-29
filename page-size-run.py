815925a8477465c73662be34f98e74f70b56245a
Marcus Müller <marcus.mueller@cit.tum.de>
2023-09-29 12:12:09 +0200

    random run

--- page-size-run.py
+++ page-size-run.py
@@ -1,35 +1,39 @@
 #!/usr/bin/env python
 
 import subprocess
+import sys
 from math import floor
+from random import choices, randrange, random
 
-
-def run(i,ycsb,data,leaf,inner,config,pl):
-    with open('page-size-cpl.csv','a') as f:
-        env = {
-            'RUN_ID':i,
-            'YCSB_VARIANT':ycsb,
-            'SCAN_LENGTH':100,
-            'OP_COUNT':'1e7',
-            'DATA': data,
-            'KEY_COUNT': floor(3e8 / ( { 'data/urls': 62.280,'data/wiki' : 22.555,'int': 4}[data] + pl )),
-            'PAYLOAD_SIZE':pl,
-            'ZIPF':-1,
-        }
-        env = {k:str(env[k]) for k in env}
-        path=f'page-size-builds/-DPS_I={inner} -DPS_L={leaf}/{config}-n3-ycsb'
-        result = subprocess.run(path, stdout=f, env=env)
-        if result.returncode!=0:
-            print(path,env,result)
-
-for i in range(1000):
-    for ycsb in [3,5]:
-        for data in ['int','data/urls','data/wiki']:
-            lower = 8 if data=='int' else 10
-            for exp in range(lower,16):
-                for pl in range(8,100,8):
-                    size = 2**exp
-                    for config in 'dense1 hash hints'.split():
-                        if config=='dense1' and data!='int':
-                            continue
-                        run(i,ycsb,data,2**exp,4096,config,pl)
+run_id = 0
+while (True):
+    run_id += 1;
+    ycsb = choices(population=[3, 5], weights=[3, 1])[0]
+    data = choices(population=['int', 'data/urls', 'data/wiki'])[0]
+    lower = 8 if data == 'int' else 10
+    psl_exp = randrange(lower, 16)
+    psl = 2 ** psl_exp
+    pl = randrange(0, 256)
+    density = 1 / 2 ** (random() * 4)
+    config = choices(population='dense1 hash hints'.split())[0]
+    if config == 'dense1' and data != 'int':
+        continue
+    env = {
+        'RUN_ID': run_id,
+        'YCSB_VARIANT': ycsb,
+        'SCAN_LENGTH': 100,
+        'OP_COUNT': '1e7',
+        'DATA': data,
+        'KEY_COUNT': floor(3e8 / ({'data/urls': 62.280, 'data/wiki': 22.555, 'int': 4}[data] + pl)),
+        'PAYLOAD_SIZE': pl,
+        'ZIPF': -1,
+        'DENSITY': density,
+    }
+    env = {k: str(env[k]) for k in env}
+    path = f'page-size-builds/-DPS_I={psl} -DPS_L=4096/{config}-n3-ycsb'
+    try:
+        print(path, env)
+        with open('page-size-random-1.csv', 'a') as f:
+            subprocess.run(path, stdout=f, env=env, check=True, timeout=120)
+    except Exception as e:
+        print(path, env, e, file=sys.stderr)
