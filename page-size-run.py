aa64e64eb0cbdc5a89ec6e8675c4cd24afe75218
Marcus Müller <marcus.mueller@cit.tum.de>
2023-10-06 10:42:00 +0200

    parallel random runs

--- page-size-run.py
+++ page-size-run.py
@@ -1,27 +1,50 @@
 #!/usr/bin/env python
-
+import fcntl
 import subprocess
 import sys
 from math import floor, log10
 from random import choices, randrange, random
 from uuid import uuid4
 
+STDOUT = 'random-1.csv'
+STDERR = f'errlog-{STDOUT}'
+
+
+# parallel -j16 -- ./page-size-run.py {1} ::: $(seq 1 16)
+
+def append(path, content):
+    if content == '':
+        return
+    with open(path, 'a') as f:
+        fcntl.lockf(f, fcntl.LOCK_EX)
+        try:
+            try:
+                f.write(content)
+            finally:
+                f.flush()
+        finally:
+            fcntl.lockf(f, fcntl.F_UNLCK)
+
+
 while (True):
     ycsb = choices(population=[3, 5], weights=[3, 1])[0]
-    data = choices(population=['int', 'data/urls', 'data/wiki'], weights=[1,2, 2])[0]
+    data = choices(population=['int', 'data/urls', 'data/wiki'], weights=[1, 1, 1])[0]
     avg_key_size = {'data/urls': 62.280, 'data/wiki': 22.555, 'int': 4}[data]
     max_key_count = {'data/urls': 6300000, 'data/wiki': 15000000, 'int': 4e9}[data]
     lower = 8 if data == 'int' else 10
     psl_exp = 12  # randrange(lower, 16)
     psl = 2 ** psl_exp
-    pl = randrange(0, 256)
-    target_total_size = 10 ** (random()*2.5+6)
+    pl = randrange(0, 17)
+    target_total_size = 10 ** (random() * 2.5 + 6)
     density = 1 / 2 ** random()
-    key_count = floor(target_total_size / (pl+avg_key_size))
-    if key_count>max_key_count:
+    key_count = floor(target_total_size / (pl + avg_key_size))
+    if key_count > max_key_count:
         continue
-    config = choices(population='dense1 hash hints dense2 baseline prefix'.split())[0]
+    # config = choices(population='dense1 hash hints dense2 baseline prefix'.split())[0]
+    config = choices(population='hash hints'.split())[0]
     if (config == 'dense1' or config == 'dense2') and data != 'int':
+        continue
+    if config == 'dense1':
         continue
     env = {
         'RUN_ID': uuid4(),
@@ -37,8 +60,13 @@ while (True):
     env = {k: str(env[k]) for k in env}
     path = f'page-size-builds/-DPS_I=4096 -DPS_L={psl}/{config}-n3-ycsb'
     try:
-        print(path, env)
-        with open('page-size-random-4.csv', 'a') as f:
-            subprocess.run(path, stdout=f, env=env, check=True, timeout=120)
+        process = subprocess.Popen(path, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env, text=True)
+        stdout, stderr = process.communicate()
+        if process.returncode != 0:
+            raise f'nonzero return code: {process.returncode}, stderr: {stderr}'
     except Exception as e:
-        print(path, env, e, file=sys.stderr)
+        process.kill()
+        stdout = ''
+        stderr = f'exception: {e}\n'
+    append(STDOUT, stdout)
+    append(STDERR, f'{env}{stderr}')
