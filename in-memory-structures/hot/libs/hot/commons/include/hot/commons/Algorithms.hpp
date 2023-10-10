c5b3f610900c4e14a37d0cdc64a66afab9f5071a
Marcus Müller <marcus.mueller@cit.tum.de>
2023-10-10 09:01:54 +0200

    replace _mm_set_pi64x with casts, clang seems to not define it.
    [This is what gcc does](https://stackoverflow.com/a/9061485):
    ```
    extern __inline __m64  __attribute__((__gnu_inline__, __always_inline__, __artificial__))
    _mm_set_pi64x (long long __i)
    {
      return (__m64) __i;
    }
    ```

--- in-memory-structures/hot/libs/hot/commons/include/hot/commons/Algorithms.hpp
+++ in-memory-structures/hot/libs/hot/commons/include/hot/commons/Algorithms.hpp
@@ -9,7 +9,7 @@
 namespace hot { namespace commons {
 
 inline uint32_t getBytesUsedInExtractionMask(uint64_t successiveExtractionMask) {
-	uint32_t const unsetBytes = _mm_movemask_pi8(_mm_cmpeq_pi8(_mm_and_si64(_mm_set_pi64x(successiveExtractionMask), _mm_set_pi64x(UINT64_MAX)), _mm_setzero_si64()));
+	uint32_t const unsetBytes = _mm_movemask_pi8(_mm_cmpeq_pi8(_mm_and_si64((__m64)(successiveExtractionMask), (__m64)(UINT64_MAX)), _mm_setzero_si64()));
 	//8 - numberUnsetBytes
 	return 8 - _mm_popcnt_u32(unsetBytes);
 }
