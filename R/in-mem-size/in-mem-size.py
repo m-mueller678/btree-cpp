8fe580b518c43e64b0ba007a350cf014c9b3b223
m-mueller678 <marcus.mueller.678@gmail.com>
2024-09-12 11:24:50 +0200

    add lits data and in-mem-size script

---
+++ R/in-mem-size/in-mem-size.py
@@ -0,0 +1,5 @@
+for d in [['int', 25_000_000], ['rng4', 25_000_000], ['data/urls-short', 4273260], ['data/wiki', 9818360]]:
+    for config in ["baseline", "hot", "art", "tlx", "adapt2",'wh','lits']:
+        path = (f'named-build/{config}-n3-ycsb')
+        command = f'env LD_LIBRARY_PATH=. MEM_SIZE=1 DATA={d[0]} KEY_COUNT={d[1]} YCSB_VARIANT=6 SCAN_LENGTH=50 RUN_ID=0 OP_COUNT=5e6 PAYLOAD_SIZE=8 ZIPF=0.99 DENSITY=1 {path}'
+        print(f'while ! {command} ; do :; done')
